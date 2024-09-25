aa6b4023818a807c56dc4f33a0617009eb41492c
Jamieee! <93412639+JamieFromNZ@users.noreply.github.com>
2024-09-25 13:49:27 +1200

    added blobs this is so cool

--- app.js
+++ app.js
@@ -11,20 +11,32 @@ app.set('views', './views');
 // Set the (static) public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+function getBlobs(number) {
+    let blobs = [];
+
+    for (let i = 0; i < number; i++) {
+        let randomTop = Math.floor(Math.random() *1000);
+        let randomLeft = Math.floor(Math.random() * 1500);
+        blobs.push({ top: randomTop, left: randomLeft });
+    }
+
+    return blobs
+}
+
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', { blobs: getBlobs(10) });
 });
 
 app.get('/about', (req, res) => {
-    res.render('about');
+    res.render('about', { blobs: getBlobs(10) });
 });
 
 app.get('/collaborate', (req, res) => {
-    res.render('collaborate');
+    res.render('collaborate', { blobs: getBlobs(10) });
 });
 
 app.get('/login', (req, res) => {
-    res.render('login');
+    res.render('login', { blobs: getBlobs(10) });
 });
 
 let port = 8080;
